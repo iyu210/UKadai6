3a34c935660a4e86dfedd190c0c10fe066574f7f
iyu210 <iyu@live.jp>
2022-02-03 22:14:18 +0900

    Initial Commit

---
+++ UKadai6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UKadai6
+//
+//  Created by 岩渕優児 on 2022/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
